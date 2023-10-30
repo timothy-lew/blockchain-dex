187188ffc8f112ddf2fbdd130e2a0f6d2280a07c
pranwanth <pranwanth@gmail.com>
2023-10-31 00:46:02 +0800

    added metamask wallet connect

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,12 +1,33 @@
 import React from 'react'
+import { useAccount, useConnect, useDisconnect } from 'wagmi'
+import { InjectedConnector } from 'wagmi/connectors/injected'
+
+import Metamask from '../assets/Metamask.svg'
+import Disconnect from '../assets/Disconnect.svg'
 
 function Navbar() {
+  const { address, isConnected } = useAccount()
+  const { connect } = useConnect({
+    connector: new InjectedConnector(),
+  })
+  const { disconnect } = useDisconnect()
   return (
-    <div className="bg-base min-w-full py-8 px-12 flex flex-row justify-between">
+    <div className="bg-base min-w-full flex flex-row justify-between items-center px-12 py-6">
       <p>Some Logo</p>
-      <button className="bg-gradient-to-l from-buttonBlue from-0% to-buttonPurple to-100% font-bold p-4 text-base rounded-2xl">
-        Connect Wallet
-      </button>
+      {!isConnected && (
+        <button onClick={() => connect()} className="bg-gradient-to-l from-buttonBlue from-0% to-buttonPurple to-100% font-bold p-4 text-base rounded-2xl">
+          Connect Wallet
+        </button>
+      )}
+      {isConnected && (
+        <div className='bg-secondary h-full flex flex-row items-center p-3 rounded'>
+          <img src={Metamask} width="24px" />
+          <div className="ml-2 font-bold">{address.slice(0, 6)}...{address.slice(address.length - 5)}</div>
+          <button className="ml-2" onClick={() => disconnect()}>
+            <img src={Disconnect} />
+          </button>
+        </div>
+      )}
     </div>
   )
 }
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,13 +1,29 @@
-import React from "react"
-import ReactDOM from "react-dom/client"
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { WagmiConfig, createConfig, configureChains, sepolia } from 'wagmi'
+import { publicProvider } from 'wagmi/providers/public'
 
 import App from './App'
 import './index.css'
+
+
+const { chains, publicClient, webSocketPublicClient } = configureChains(
+  [sepolia],
+  [publicProvider()],
+)
+
+const config = createConfig({
+  autoConnect: true,
+  publicClient,
+  webSocketPublicClient,
+})
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 
 root.render(
   <React.StrictMode>
-    <App />
+    <WagmiConfig config={config}>
+      <App />
+    </WagmiConfig>
   </React.StrictMode>
 )
